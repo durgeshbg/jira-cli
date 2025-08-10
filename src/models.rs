9c0700bdb985c71f4ea1826000b531af5fc3bf68
Durgesh <84910181+Durgesh-babu@users.noreply.github.com>
2025-08-10 16:01:45 +0530

    Add models module with Status, Epic, Story, and DBState structures

---
+++ src/models.rs
@@ -0,0 +1,48 @@
+use std::collections::HashMap;
+
+pub enum Status {
+    Open,
+    InProgress,
+    Resolved,
+    Closed,
+}
+
+pub struct Epic {
+    pub name: String,
+    pub description: String,
+    pub status: Status,
+    pub stories: Vec<i32>,
+}
+
+impl Epic {
+    pub fn new(name: String, description: String) -> Self {
+        Self {
+            name,
+            description,
+            status: Status::Open,
+            stories: vec![],
+        }
+    }
+}
+
+pub struct Story {
+    pub name: String,
+    pub description: String,
+    pub status: Status,
+}
+
+impl Story {
+    pub fn new(name: String, description: String) -> Self {
+        Self {
+            name,
+            description,
+            status: Status::Open,
+        }
+    }
+}
+
+pub struct DBState {
+    pub last_item_id: i32,
+    pub stories: HashMap<i32, Story>,
+    pub epics: HashMap<i32, Epic>,
+}
